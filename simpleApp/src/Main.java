6b906237641d95bf41d8478b2d2b05ca9639e8be
iamnikitamallya <mallyanikita165@gmail.com>
2024-06-11 01:07:02 +0100

    adding small features

--- simpleApp/src/Main.java
+++ simpleApp/src/Main.java
@@ -8,5 +8,13 @@ public class Main {
             String num = "i = " + i;
             System.out.println(num);
         }
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        int sum = num1 + num2 + num3;
+
+        System.out.print("Sum of 3 numbers is "+ sum);
     }
 }
